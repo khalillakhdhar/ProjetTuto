501e5be57a2b820b2f5298bd576479bfeb7c5b4d
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-22 10:40:50 +0100

    myclasses

--- src/classes/listes/Malades.java
+++ src/classes/listes/Malades.java
@@ -1,5 +1,8 @@
 package classes.listes;
+import java.util.ArrayList;
 
+import classes.Malade;
 public class Malades {
+public static ArrayList<Malade> maladesList=new ArrayList<Malade>();
 
 }
---
+++ src/classes/listes/Parents.java
@@ -0,0 +1,15 @@
+package classes.listes;
+
+import java.util.ArrayList;
+
+import classes.Parent;
+
+public class Parents {
+public static ArrayList<Parent> parentsListe=new ArrayList<Parent>();
+public void addToList(Parent p)
+{
+parentsListe.add(p);	
+}
+
+
+}
