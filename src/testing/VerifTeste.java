9b8a88f557e9bb0c659a8eb336b934cc57a0b9b1
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-22 14:21:57 +0100

    verifdate

---
+++ src/testing/VerifTeste.java
@@ -0,0 +1,10 @@
+package testing;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class VerifTeste {
+private List<Integer> big= Arrays.asList(1,3,5,7,8,10,12);
+private List<Integer> small=Arrays.asList(4,6,9,11);
+
+}
