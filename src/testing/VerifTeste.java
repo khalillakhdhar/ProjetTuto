ede32446ae987a143c233b427c6f4a4176c0ebfb
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-22 14:27:03 +0100

    dates

--- src/testing/VerifTeste.java
+++ src/testing/VerifTeste.java
@@ -6,5 +6,33 @@ import java.util.List;
 public class VerifTeste {
 private List<Integer> big= Arrays.asList(1,3,5,7,8,10,12);
 private List<Integer> small=Arrays.asList(4,6,9,11);
+public boolean valideJour(int jour)
+{
+return jour>0 && jour<=31;
+
+}
+public boolean validMois(int month)
+{
+return month<=12 && month>0;	
+}
+public boolean validDay(int day,int month)
+{
+if(!valideJour(day)||!validMois(month))
+	return false;
+else if(small.contains(month)&&day >30)
+	return false;
+else if(month==2 && day>29)
+	return false;
+else
+	return true;
+
+
+
+}
+
+
+
+
+
 
 }
