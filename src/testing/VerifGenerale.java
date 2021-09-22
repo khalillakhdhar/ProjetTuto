71c8ef975c95d84802bad30b2730fd145d4c8390
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-22 11:27:45 +0100

    teste

--- src/testing/VerifGenerale.java
+++ src/testing/VerifGenerale.java
@@ -6,4 +6,11 @@ public boolean verifChaine(String x)
 return !x.isEmpty();	
 
 }
+public double conversibleDouble(String y)
+{
+	//conversion de String en double
+	//si y="21,3" => return =21,3
+	//si y="21a,3" => return exception 
+return Double.parseDouble(y);	
+}
 }
