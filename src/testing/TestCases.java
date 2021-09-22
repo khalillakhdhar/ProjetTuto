0ee83165da85b971fa77731582424ae1195f1aa6
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-22 14:59:32 +0100

    assertions

--- src/testing/TestCases.java
+++ src/testing/TestCases.java
@@ -1,6 +1,10 @@
 package testing;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 public class TestCases {
@@ -9,8 +13,26 @@ public ExpectedException exception=ExpectedException.none();
 public VerifAdmin va;
 public VerifGenerale vg;
 public VerifTeste vt;
+@Before
+public void beforeCoding()
+{
+this.va=new VerifAdmin();
+this.vg=new VerifGenerale();
+this.vt=new VerifTeste();
 
+}
+@Test
+public void testInt()
+{
 	
+exception.expect(NumberFormatException.class);
+vg.convertToInt("11a");
+}
+@Test
+public void testPositif()
+{
+assertTrue(vg.verifPositif(4));	
+}
 	
 	
 	
