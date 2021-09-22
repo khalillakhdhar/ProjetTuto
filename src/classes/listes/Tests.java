5d40d9ea8f0befc67dae01fa0ac353c8e839935d
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-22 10:51:49 +0100

    teste

---
+++ src/classes/listes/Tests.java
@@ -0,0 +1,21 @@
+package classes.listes;
+
+import java.util.ArrayList;
+import classes.Test;
+public class Tests {
+public static ArrayList<Test> testList=new ArrayList<Test>();
+
+public void addtoTests(Test t)
+{
+testList.add(t);	
+
+}
+public static void showlist()
+{
+for(Test tes:testList)
+	System.out.println(tes.toString());
+
+}
+
+
+}
